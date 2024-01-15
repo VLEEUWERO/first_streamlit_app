a4b770184d75b89836ac61c13f0c82cba4b7b257
VLEEUWERO <156658682+VLEEUWERO@users.noreply.github.com>
2024-01-15 10:59:42 +0100

    Update streamlit_app.py
    - updated the menu text with icons, and added 1 menu line text;
    - started a comment with all unique icons in use for a menu section

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,8 @@ import streamlit
 streamlit.title('My Parents new Healthy Diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinack & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinack & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
+# comment with all unique symbols used in section above: '🥣 🥗 🐔 🥑🍞'
