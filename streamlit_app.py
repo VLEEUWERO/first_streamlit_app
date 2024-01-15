8a4a82f8f3fb8df3224ae3831d1c732e590ac214
VLEEUWERO <156658682+VLEEUWERO@users.noreply.github.com>
2024-01-15 11:11:07 +0100

    Update streamlit_app.py
    updated streamlit to st, and hidden the index for the dataframe of smoothie ingredients

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,22 +1,21 @@
-import streamlit
+import streamlit as st
 import pandas as pd
 
-streamlit.title('My Parents new Healthy Diner')
+st.title('My Parents new Healthy Diner')
 
-streamlit.header('Breakfast Menu')
-streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
-streamlit.text('🥗 Kale, Spinack & Rocket Smoothie')
-streamlit.text('🐔 Hard-Boiled Free-Range Egg')
-streamlit.text('🥑🍞 Avocado Toast')
+st.header('Breakfast Menu')
+st.text('🥣 Omega 3 & Blueberry Oatmeal')
+st.text('🥗 Kale, Spinack & Rocket Smoothie')
+st.text('🐔 Hard-Boiled Free-Range Egg')
+st.text('🥑🍞 Avocado Toast')
 # comment with all unique symbols used in section above: '🥣 🥗 🐔 🥑🍞'
 
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-#import pandas as pd
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(my_fruit_list)
+st.dataframe(my_fruit_list, hide_index=True)
 
 # Let's put a pick list here so they can pick the fruit they want to include 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+st.multiselect("Pick some fruits:", list(my_fruit_list.index))
 # Display the table on the page.
 
