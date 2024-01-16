0f44b40f4dffa9ea6d55058ace0d62be14148c28
VLEEUWERO <156658682+VLEEUWERO@users.noreply.github.com>
2024-01-16 16:50:18 +0100

    Update streamlit_app.py
    add a control flow issue

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,3 +49,7 @@ st.dataframe(my_data_rows, hide_index=True)
 #Allow the end user to add a fruit to the list
 add_my_fruit = st.text_input('What fruit would you like to add?','jackfruit')
 st.write('Thanks for adding ', add_my_fruit)
+
+
+#this will not work correctly, but just go with it for now
+my_cur.execute("insert into fruit_load_list values ('from st');")
